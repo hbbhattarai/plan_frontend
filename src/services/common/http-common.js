af03a438a7c1bf7da920ca7e7788f046db475459
hembbcb <hemchettri56.geo@gmail.com>
2022-06-23 16:17:25 +0600

    ADD

--- src/services/common/http-common.js
+++ src/services/common/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://192.168.20.87:5000",
   headers: {
     "Content-type": "application/json"
   }
